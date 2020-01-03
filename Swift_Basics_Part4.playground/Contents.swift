cdab0998c5778b02256371c7e1f9ebcf89d91c06
Virendra Gupta <vgupta@ivycomptech.com>
2020-01-03 12:33:42 +0530

    second commit

--- Swift_Basics_Part4.playground/Contents.swift
+++ Swift_Basics_Part4.playground/Contents.swift
@@ -1,3 +1,26 @@
+/* This playground contains explanation and code for the below topics in Swift 5.1
+1.String and characters
+ */
+
 import UIKit
+
+/* Swift's String type is bridged with Foundation's NSString class.
+   You can access those NSString methods on String without casting
+ */
+
+let someString = "Some string literal value"
+
+//multiline string literal
+let quatation = """
+ababbjkand
+ad jkasb dkjas
+
+ad kjas das
+
+akd a
+
+adajdanjdnjdbakd.
+"""
+
 
 
