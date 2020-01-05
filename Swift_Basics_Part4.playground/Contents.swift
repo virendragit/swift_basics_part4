8e1e71cab485f28f0f8370165db470f29aa6daae
Virendra Gupta <vgupta@ivycomptech.com>
2020-01-05 11:54:42 +0530

    string utf8 uth16 added

--- Swift_Basics_Part4.playground/Contents.swift
+++ Swift_Basics_Part4.playground/Contents.swift
@@ -100,6 +100,109 @@ instruction += string2
 
 welcome.append(exclamationMark)
 
+//String Interpolation (String Mix of constants, veriables ,literals, and expressions)
+
+let multiplier = 3.0
+let message = "\(multiplier) times 2.5 is \(multiplier * 2.5)"
+
+print(#"6 times 7 is \#(6 * 7)."#)
+
+//Accessing and modifying a string
+
+let greeting = "Guten Tag!"
+greeting[greeting.startIndex]
+print("end Index ",greeting[greeting.index(before: greeting.endIndex)])
+print(greeting[greeting.index(after: greeting.startIndex)])
+
+let index = greeting.index(greeting.startIndex, offsetBy: 7)
+greeting[index]
+
+//Inserting and Removing
+
+var welcome1 = "hello"
+welcome1.insert("!", at: welcome1.endIndex)
+
+welcome1.remove(at: welcome1.index(before: welcome1.endIndex))
+print(welcome1)
+
+//you can use insert(_:at:), insert(contentOf:at:), remove(at:), and removeSubrange(_:)
+//methods on any type that conforms to RangeReplaceableCollection protocol.
+//This includes String and other collection types such as Array, Dictionary and Set.
+
+//Substring
+let greeting1 = "Hello, world"
+let indexOfchar = greeting1.firstIndex(of: ",") ?? greeting1.endIndex
+print(indexOfchar)
+
+let beginning = greeting1[..<indexOfchar]
+
+//convert the result to a String for the long-term storage.
+
+let newString = String(beginning)
+
+//String and Character Equality.
+//String and character in swift are not locale-sensitive.
+
+//Prefix and Suffix Equality
+
+let romeoAndJuliet = [
+    "Act 1 Scene 1: Verona, A public place",
+    "Act 1 Scene 2: Capulet's mansion",
+    "Act 1 Scene 3: A room in Capulet's mansion",
+    "Act 1 Scene 4: A street outside Capulet's mansion",
+    "Act 1 Scene 5: The Great Hall in Capulet's mansion",
+    "Act 2 Scene 1: Outside Capulet's mansion",
+    "Act 2 Scene 2: Capulet's orchard",
+    "Act 2 Scene 3: Outside Friar Lawrence's cell",
+    "Act 2 Scene 4: A street in Verona",
+    "Act 2 Scene 5: Capulet's mansion",
+    "Act 2 Scene 6: Friar Lawrence's cell"
+]
+
+var act1SceneCount = 0
+for scene in romeoAndJuliet{
+    
+    if scene.hasPrefix("Act 1"){
+        
+        act1SceneCount += 1
+    }
+}
+
+print("There are \(act1SceneCount) scenes in Act 1")
+
+var mansionCount = 0
+var cellCount = 0
+for scene in romeoAndJuliet {
+    if scene.hasSuffix("Capulet's mansion"){
+        mansionCount += 1
+    }else if scene.hasSuffix("Friar Lawrence's cell"){
+        cellCount += 1
+    }
+}
+
+print("\(mansionCount) mansion scenes; \(cellCount) cell scenes")
+
+//Unicode representation of Strings
+
+let dogStrin = "Dog!!🐶"
+
+for codeUnit in dogStrin.utf8{
+    print("\(codeUnit) ", terminator:" ")
+}
+
+for codeUnit in dogStrin.utf16{
+    print("\n \(codeUnit) ", terminator:" ")
+}
+
+
+for scalar in dogStrin.unicodeScalars{
+    print("\(scalar)")
+}
+
+
+
+
+
 
 
 
